b64473ae82f515b1fc12924d311fc38028955fe7
Anwar20233 <a.nwar1@hotmail.com>
2026-06-06 23:19:06 +0300

    Fix MySQL connection in setup.php

--- setup.php
+++ setup.php
@@ -1,7 +1,7 @@
 <?php
 $conn = mysqli_connect(
-    getenv('MYSQLHOST'), getenv('MYSQLUSER'),
-    getenv('MYSQLPASSWORD'), getenv('MYSQLDATABASE'), getenv('MYSQLPORT')
+    'mysql.railway.internal', 'root',
+    'FqUOVLFTEMdZftUQoNkGuPgGGWfvytNR', 'railway', 3306
 );
 if (!$conn) die('Connection failed: ' . mysqli_connect_error());
 
