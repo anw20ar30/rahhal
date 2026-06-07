c636cd9a9f6be2ad6e2b232de615b8aa4f1b01ef
Anwar20233 <a.nwar1@hotmail.com>
2026-06-07 19:19:27 +0300

    Replace sample experiences with curated international stays and remove team section from about page

--- pages/about.php
+++ pages/about.php
@@ -19,13 +19,6 @@ $values = [
     ['icon' => 'fas fa-headset', 'title' => 'الدعم المستمر', 'text' => 'فريقنا حاضر لمساعدة الضيوف قبل الرحلة وأثناءها وبعدها.'],
 ];
 
-$team = [
-    ['name' => 'أحمد الشمري', 'role' => 'المؤسس والرئيس التنفيذي', 'image' => 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=300&q=80'],
-    ['name' => 'سارة الحربي', 'role' => 'مديرة تجربة العملاء', 'image' => 'https://images.unsplash.com/photo-1494790108755-2616b612b786?w=300&q=80'],
-    ['name' => 'محمد القحطاني', 'role' => 'مدير التقنية', 'image' => 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=300&q=80'],
-    ['name' => 'نورة العتيبي', 'role' => 'مديرة التسويق', 'image' => 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=300&q=80'],
-];
-
 include '../includes/header.php';
 ?>
 
@@ -82,23 +75,6 @@ include '../includes/header.php';
         <div class="feat-ico"><i class="<?= htmlspecialchars($value['icon']) ?>"></i></div>
         <h4 class="feat-title"><?= htmlspecialchars($value['title']) ?></h4>
         <p class="feat-desc"><?= htmlspecialchars($value['text']) ?></p>
-      </div>
-      <?php endforeach; ?>
-    </div>
-
-    <div class="text-center mb-5 mt-5">
-      <div class="sec-eyebrow"><i class="fas fa-users"></i> فريقنا</div>
-      <h2 class="sec-title">من يقود رحّال؟</h2>
-      <p class="sec-sub mx-auto">فريق يهتم بالضيافة، التفاصيل، وتجربة المستخدم العربية.</p>
-    </div>
-    <div class="row g-4 justify-content-center">
-      <?php foreach ($team as $member): ?>
-      <div class="col-6 col-md-3">
-        <div class="team-card">
-          <img src="<?= htmlspecialchars($member['image']) ?>" alt="<?= htmlspecialchars($member['name']) ?>">
-          <h5><?= htmlspecialchars($member['name']) ?></h5>
-          <p><?= htmlspecialchars($member['role']) ?></p>
-        </div>
       </div>
       <?php endforeach; ?>
     </div>
